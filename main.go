a86fe0745af98293f094718579fd7efe908c0e7e
wafflydevin <devin.cheriant@gmail.com>
2024-12-03 18:09:00 -0500

    added custom hsndler, changed site path to be /app/ then stripped it so code can find correct directory

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -18,4 +20,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
