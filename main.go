a42e78830106fdfff7e090442d93a1110d44b7d2
wafflydevin <devin.cheriant@gmail.com>
2024-12-03 11:28:27 -0500

    added image asset, moved filepathRoot directory to a const

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
